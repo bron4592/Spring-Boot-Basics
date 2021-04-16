d9c211b0ad6b15137d4e99e6ca2feab091044556
Brandon ONeill <bron4592@colorado.edu>
2021-04-15 19:16:57 -0800

    First commit

--- demo/src/main/java/com/example/demo/student/StudentConfig.java
+++ demo/src/main/java/com/example/demo/student/StudentConfig.java
@@ -1,2 +1,31 @@
-package com.example.demo.student;public class StudentConfig {
+package com.example.demo.student;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.time.LocalDate;
+import java.time.Month;
+import java.util.List;
+
+@Configuration
+public class StudentConfig {
+
+    @Bean
+    CommandLineRunner commandLineRunner(StudentRepository repository){
+        return args -> {
+           Student john = new Student(
+              "John",
+              "johndoe@gmail.com",
+              LocalDate.of(2000, Month.JANUARY, 5)
+            );
+            Student jane = new Student(
+                    "Jane",
+                    "janedoe@gmail.com",
+                    LocalDate.of(2001, Month.JANUARY, 15)
+            );
+
+            repository.saveAll(List.of(john, jane));
+        };
+    }
 }
